df8d4b44a59c646d515b491fd169133095d75c58
Mainur Rahman <enan.mainur@gmail.com>
2022-01-02 10:36:15 +0600

    changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import AddProduct from './Pages/AddProduct/AddProduct';
 
 function App() {
   return (
     <div className="App">
       <h1>Online Free Auction</h1>
+      <hr/>
+      {/* This is Mainur Rahman */}
     </div>
   );
 }
